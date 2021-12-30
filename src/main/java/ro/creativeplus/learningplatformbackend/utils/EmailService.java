1f28945d60eefe1a2a71730a0eadc0257db115cc
Haginus <hagiandrei.ah@gmail.com>
2021-12-30 02:28:13 +0200

    Send emails

---
+++ src/main/java/ro/creativeplus/learningplatformbackend/utils/EmailService.java
@@ -0,0 +1,53 @@
+package ro.creativeplus.learningplatformbackend.utils;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+import ro.creativeplus.learningplatformbackend.model.User.User;
+import ro.creativeplus.learningplatformbackend.model.User.UserActivationToken;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class EmailService {
+
+  private final JavaMailSender javaMailSender;
+
+  EmailService(JavaMailSender javaMailSender) {
+    this.javaMailSender = javaMailSender;
+  }
+
+  public void sendSimpleMessage(String to, String subject, String text) {
+    SimpleMailMessage message = new SimpleMailMessage();
+    message.setFrom("change.this.later@example.org");
+    message.setTo(to);
+    message.setSubject(subject);
+    message.setText(text);
+    javaMailSender.send(message);
+  }
+
+  public void sendHtmlMessage(String to, String subject, String html) {
+    try {
+      MimeMessage message = javaMailSender.createMimeMessage();
+      message.setFrom("change.this.later@example.org");
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+      message.setSubject(subject);
+      message.setContent(html, "text/html");
+      javaMailSender.send(message);
+    } catch (MessagingException err) {
+      throw new RuntimeException("Email not sent.");
+    }
+  }
+
+  public void sendWelcomeMessage(User user, UserActivationToken token) {
+    // This is junk, we'll come back later to this.
+    String html = "<html>" +
+        "Hello, " + user.getFirstName() + " " + user.getLastName() + "!<br>" +
+        "Here is your token: " + token.getToken() +
+        "</html>";
+    sendHtmlMessage(user.getEmail(), "Welcome to the training platform!", html);
+  }
+}
